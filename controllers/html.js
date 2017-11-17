c2208b5315816fb046b57ebb4a84bf11926f6dfc
BenHigh <ben.high@austin.rr.com>
2017-11-17 13:03:00 -0600

    finishing up app structure

--- controllers/html.js
+++ controllers/html.js
@@ -1,7 +1,9 @@
+var express = require("express");
+
+var router = express.Router();
+
 module.exports = {
   renderHome: function(req, res) {
-    res.render('index', {
-      msg: "Welcome! this is data being passed into the render function!"
-    });
+    res.render('index');
   }
 };
