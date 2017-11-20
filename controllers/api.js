25b28c8f5b7e465a334d9066898aaaf9bd5e8df2
Thomas Wood <twoodou@outlook.com>
2017-11-20 10:19:43 -0600

    updated api.js to include 'personality_insights var

--- controllers/api.js
+++ controllers/api.js
@@ -1,3 +1,22 @@
+var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
+
+var personality_insights = new PersonalityInsightsV3({
+  username: "7ad6cee6-aab9-4f90-b17e-734d63050359",
+  password: "tFUZRAZdCKJo",
+  version_date: '2016-10-19'
+});
+
+personality_insights.profile({
+  text: 'Enter more than 100 unique words here...',
+  consumption_preferences: true
+  },
+  function (err, response) {
+    if (err)
+      console.log('error:', err);
+    else
+      console.log(JSON.stringify(response, null, 2));
+});
+
 module.exports = {
   index: function(req, res) {
     res.status(200).json({
