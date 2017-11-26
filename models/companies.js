a70b09116fe5ed2cc9654f930652a2d88fd7d548
Thomas Wood <twoodou@outlook.com>
2017-11-26 17:32:57 -0600

    changed config.json and package.json for PostgreSQL addition/change.

---
+++ models/companies.js
@@ -0,0 +1,27 @@
+module.exports = function(sequelize, DataTypes) {
+  var Company = sequelize.define("Company", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    body: {
+      type: DataTypes.JSONB,
+      allowNull: false
+    }
+  });
+
+  // Post.associate = function(models) {
+  //   // We're saying that a Post should belong to an Author
+  //   // A Post can't be created without an Author due to the foreign key constraint
+  //   Post.belongsTo(models.Author, {
+  //     foreignKey: {
+  //       allowNull: false
+  //     }
+  //   });
+  // };
+
+  return Company;
+};
